a76e0b885e9feae5cfc15fc3c86b97a1b12b7498
celalcan <celalcan@users.noreply.github.com>
2022-10-25 15:51:25 +0300

    Add files via upload

---
+++ ES6-Alistirma/es6-deneme-ornekleri.js
@@ -0,0 +1,41 @@
+/* console.log(this);
+
+const emp1={
+    name: "mustafa",
+    age: 25,
+    ShowInfos:function() {console.log("Bilgiler Gösteriliyor...");}
+};
+
+const emp2={
+    name: "Ahmet",
+    age: 25
+}
+
+console.log(emp1); */
+
+// function Employee/* Büyük harfle başlaması önemli */(name, age, salary) { //yapıcı fonksiyon - Constructor
+//     this.name = name;
+//     this.age = age;
+//     this.salary = salary;
+
+//     this.showInfos = function() {
+//         console.log(this.name, this.age, this.salary);
+//     }
+// }
+
+// const emp1 = new Employee("celalcan", 25, 4000);
+// const emp2 = new Employee("hacıhasanoğlu", 25, 5000);
+
+// emp1.showInfos();
+// emp2.showInfos();
+
+
+
+
+
+/* PROTOTYPE-1 */
+
+
+
+
+
